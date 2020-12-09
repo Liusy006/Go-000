b60c1b86b27433816f6512591e4e989e5d3b9a68
Yoo <liusy_cn@163.com>
2020-12-09 22:52:11 +0800

    feature: week3 homework

---
+++ Week03/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"golang.org/x/sync/errgroup"
+	"net/http"
+	"os"
+	"os/signal"
+)
+
+func main(){
+	signalChan := make(chan os.Signal)
+	signal.Notify(signalChan)
+	stop := make(chan string)
+
+	g, ctx := errgroup.WithContext(context.Background())
+	g.Go(func()error{
+		defer fmt.Println("httpserver is stoping")
+		sm := http.NewServeMux()
+		sm.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("hello"))
+		})
+
+		server := http.Server{
+			Addr:              ":8080",
+			Handler:           sm,
+
+		}
+
+		errChan := make(chan string)
+		go func(){
+			defer fmt.Println("server is stop....")
+
+			err := server.ListenAndServe()
+			if err != nil{
+				errChan <- err.Error()
+			}
+		}()
+
+		errInfo := ""
+		select {
+			case <-stop:
+				server.Close()
+			case errInfo = <- errChan:
+				close(stop)
+		}
+
+		return errors.New(errInfo)
+	})
+
+	g.Go(func()error{
+		select{
+			case  s := <- signalChan:
+				close(stop)
+			return errors.New(s.String())
+			case <-stop:
+				return nil
+		}
+
+	})
+
+	fmt.Println("waiting...")
+	err := g.Wait()
+	if err != nil{
+		fmt.Println(err.Error())
+	}
+	fmt.Println(ctx.Err())
+}
